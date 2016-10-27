e5744a7669b272f40cd679bf44f022d3e7d28bf4
Daniel Pease <daniel.j.pease@gmail.com>
2016-10-27 11:06:57 -0400

    prep for user stories and whiteboarding

---
+++ app.js
@@ -0,0 +1,22 @@
+const express         = require('express');
+const logger          = require('morgan');
+const bodyParser      = require('body-parser');
+const methodOverride  = require('method-override');
+
+const app             = express();
+const PORT            = process.argv[2] || process.env.PORT || 3000;
+
+const homeRoute = require('./routes/home');
+const searchRoute = require('./routes/search');
+
+app.use(logger('dev'));
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', homeRoute);
+app.use('/search', searchRoute);
+
+app.listen(PORT, () => console.warn(`Server listening on port ${PORT}`));
---
+++ services/parks.js
@@ -0,0 +1,7 @@
+const fetch = require('node-fetch');
+
+const API_URL = 'go here';
+
+// function getParks (req, res, next) {
+//   fetch(``)
+// }
