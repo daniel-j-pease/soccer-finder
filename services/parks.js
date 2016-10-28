4f74c56e852bfcc4a88a999a95a3415d88bc4d13
Daniel Pease <daniel.j.pease@gmail.com>
2016-10-28 18:33:44 -0400

    dot env

--- services/parks.js
+++ services/parks.js
@@ -1,12 +1,15 @@
 const $ = require('jQuery');
 
+const API_URL = 'https://data.cityofnewyork.us/resource/9wwi-sb8x.json?'
+const APP_TOKEN = process.env.APP_TOKEN;
+
 $(document).ready( () => {
   $.ajax({
-    url: 'https://data.cityofnewyork.us/resource/9wwi-sb8x.json',
+    url: `${API_URL}`,
     type: 'GET',
     data: {
       $limit: 5000,
-      $$app_token: 'JeDqnn45DUDxmyCHvv7QE2VvC',
+      $$app_token: '',
     },
   })
   .done((data) => {
